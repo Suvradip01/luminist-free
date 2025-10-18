a9ffd72b3925006a732778cece4bd10c38babfb3
Suvradip01 <ghoshsuvradip215@gmail.com>
2025-10-19 02:13:36 +0530

    Initial commit of the AI Photo Editor web app

---
+++ components/header.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap">
+            <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full
+                    px-8 py-3 flex items-center justify-between gap-8">
+                <Link href="/" className="mr-10 md:mr-20">
+                    <Image
+                        src="/logo.png"
+                        alt="Pixel logo"
+                        className="min-w-24 object-cover"
+                        width={96}
+                        height={24}
+                    />
+                </Link>
+                {/* Navigation links would typically go here */}
+            </div>
+        </header>
+    );
+};
+
+export default Header;
